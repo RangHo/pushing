457cfdbf90d0b67ee122aa9786e9df925ae40540
RangHo Lee <hello@rangho.me>
2025-04-16 12:54:42 +0900

    chore: fix eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,30 @@
 import eslint from '@eslint/js';
+import tsdoceslint from 'eslint-plugin-tsdoc';
+import { globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended);
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  globalIgnores(['dist/', 'node_modules/', '*.config.js']),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+    plugins: {
+      'eslint-plugin-tsdoc': tsdoceslint,
+    },
+  }
+);
